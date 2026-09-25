7dd413569a2da903f75ad3e4d018ec0650e14e09
Dmitry Pavlenko <104529636+jokino9212@users.noreply.github.com>
2026-09-25 11:00:45 +0300

    Bump MatKvest cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "matkvest-iphone-v1";
+const CACHE_NAME = "matkvest-iphone-v2";
 const APP_FILES = ["./", "./index.html", "./manifest.webmanifest"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES)).then(() => self.skipWaiting()));
